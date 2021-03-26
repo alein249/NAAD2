e545beaf7127f10b2551a7ede86de23e92f335b4
Pankaj Sharma <panksh249@gmail.com>
2021-03-26 20:44:25 +0530

    Convert int type variable to string

---
+++ src/com/streamliners/task0/typeConversion/ConvertInt.java
@@ -0,0 +1,19 @@
+package com.streamliners.task0.typeConversion;
+
+public class ConvertInt {
+    public static void main(String[] args) {
+        // create int variable
+        int num1 = 36;
+        int num2 = 99;
+
+        // convert int to string
+        // using valueOf()
+        String str1 = String.valueOf(num1);
+        String str2 = String.valueOf(num2);
+
+        // print string variables
+        System.out.println(str1);    // 36
+        System.out.println(str2);    // 99
+
+    }
+}
