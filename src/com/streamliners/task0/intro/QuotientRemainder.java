535a1a40a85815f88846c5f5a69d03fd0de9775d
Pankaj Sharma <panksh249@gmail.com>
2021-03-26 15:40:58 +0530

    Find quotient and remainder

---
+++ src/com/streamliners/task0/intro/QuotientRemainder.java
@@ -0,0 +1,11 @@
+package com.streamliners.task0.intro;
+
+public class QuotientRemainder {
+    public static void main(String[] args) {
+        int dividend = 26,divisor = 4;
+        int quotient = dividend/divisor;
+        int remainder = dividend%divisor;
+        System.out.println("Quotient = "+ quotient);
+        System.out.println("Remainder = "+ remainder);
+    }
+}
