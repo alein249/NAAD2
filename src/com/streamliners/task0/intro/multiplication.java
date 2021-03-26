76136aa8186a683fe17c2968b40221d3acd724ad
Pankaj Sharma <panksh249@gmail.com>
2021-03-26 15:14:52 +0530

    Multiplication of two floating point numbers

---
+++ src/com/streamliners/task0/intro/multiplication.java
@@ -0,0 +1,10 @@
+package com.streamliners.task0.intro;
+
+public class multiplication {
+    public static void main(String[] args) {
+        float first = 1.5f;
+        float second = 3.2f;
+        float product = first*second;
+        System.out.println("The product is = "+product);
+    }
+}
