ffa37f3c6a06229c54e367c829a29d348a7fdcdf
Pankaj Sharma <panksh249@gmail.com>
2021-03-27 00:28:25 +0530

    Number of digits in an integer

---
+++ src/com/streamliners/task0/decisionMakingLoops/NumberOfDigits.java
@@ -0,0 +1,13 @@
+package com.streamliners.task0.decisionMakingLoops;
+
+public class NumberOfDigits {
+    public static void main(String[] args) {
+        int count = 0, num = 3452;
+
+        while (num != 0) {
+            num /= 10;
+            count++;
+        }
+        System.out.println("Number of digits: " + count);
+    }
+}
