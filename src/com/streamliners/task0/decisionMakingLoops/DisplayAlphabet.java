35c697f648627d111f141a390e5d9658a71d9e86
Pankaj Sharma <panksh249@gmail.com>
2021-03-27 00:20:42 +0530

    Display uppercased alphabets

---
+++ src/com/streamliners/task0/decisionMakingLoops/DisplayAlphabet.java
@@ -0,0 +1,10 @@
+package com.streamliners.task0.decisionMakingLoops;
+
+public class DisplayAlphabet {
+    public static void main(String[] args) {
+        char c;
+        for(c = 'A';c <= 'Z';c++){
+            System.out.print(c + " ");
+        }
+    }
+}
