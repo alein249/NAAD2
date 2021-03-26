f6229c2743a4400a79b14baa90230f82ef7ba3a5
Pankaj Sharma <panksh249@gmail.com>
2021-03-26 20:35:05 +0530

    Convert string to int

---
+++ src/com/streamliners/task0/typeConversion/ConvertString.java
@@ -0,0 +1,19 @@
+package com.streamliners.task0.typeConversion;
+
+public class ConvertString {
+    public static void main(String[] args) {
+        // create string variables
+        String str1 = "23";
+        String str2 = "4566";
+
+        // convert string to int
+        // using parseInt()
+        int num1 = Integer.parseInt(str1);
+        int num2 = Integer.parseInt(str2);
+
+        // print int values
+        System.out.println(num1);    // 23
+        System.out.println(num2);    // 4566
+
+    }
+}
