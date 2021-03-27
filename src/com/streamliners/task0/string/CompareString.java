13fa50d742f44ed68ba034518049eae414e4c250
Pankaj Sharma <panksh249@gmail.com>
2021-03-27 10:51:18 +0530

    Compare string

---
+++ src/com/streamliners/task0/string/CompareString.java
@@ -0,0 +1,14 @@
+package com.streamliners.task0.string;
+
+public class CompareString {
+    public static void main(String[] args) {
+        String style = new String("streamliner");
+        String style2 = new String("utreamliner");
+
+        if(style.equals(style2))
+            System.out.println("Equal");
+        else
+            System.out.println("Not Equal");
+    }
+}
+
