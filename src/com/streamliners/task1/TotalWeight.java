a8f684748fd61e0cc22a91515d4d37907c62d9e7
Pankaj Sharma <panksh249@gmail.com>
2021-03-28 21:35:58 +0530

    Total weight

---
+++ src/com/streamliners/task1/TotalWeight.java
@@ -0,0 +1,44 @@
+package com.streamliners.task1;
+
+import java.util.Scanner;
+
+public class TotalWeight {
+    public static void main(String[] args) {
+        //taking string as input
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string =");
+        String str = sc.nextLine();
+        //taking input in array
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("Enter no of elements in array :");
+        int n = sc1.nextInt();
+        //create an array
+        int[] weights = new int[10];
+        System.out.println("Enter the elements of array :");
+        for(int i = 0; i < n; i++){
+            //reading array elements
+            weights[i] = sc1.nextInt();
+        }
+
+        int count = weightCal(str, weights);
+        System.out.println(count);
+
+    }
+
+    public static int weightCal(String str, int[] weights){
+        int count = 0;
+        for( int i = 0;i <str.length(); i++){
+            if(str.charAt(i) == 'A')
+                count += weights[0];
+            if(str.charAt(i) == 'B')
+                count += weights[1];
+            if(str.charAt(i) == 'C')
+                count += weights[2];
+            if(str.charAt(i) == 'D')
+                count += weights[3];
+            if(str.charAt(i) == 'E')
+                count += weights[4];
+        }
+        return count;
+    }
+}
