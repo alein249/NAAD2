98d04db5a4b7253623334f970d8532fcd3da7986
Pankaj Sharma <panksh249@gmail.com>
2021-04-08 16:33:05 +0530

    Task3_2 completed

---
+++ src/com/streamliners/task3_2/GroceryItem.java
@@ -0,0 +1,11 @@
+package com.streamliners.task3_2;
+
+public class GroceryItem {
+    String name;
+    int pricePerKg;
+    //define parameterized constructor
+    public GroceryItem(String name,int pricePerKg){
+        this.name = name;
+        this.pricePerKg = pricePerKg;
+    }
+}
